ca04b26af20c2c5da9f21ffa59e912f5d5d6080e
Artem Viatkin <newyorkbox47@gmail.com>
2023-01-13 14:36:40 +0200

    commit 13.01.23 Artem

---
+++ lesson 19/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_19
+{
+    class Person 
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+
+        public string SSN { get; } = "";
+        public Person(string fName, string lName, int personaAge, string ssn)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personaAge;
+            SSN = ssn;
+        }
+        //возвратть хэш код на основе уникальных строковых данных
+        public override int GetHashCode() => SSN.GetHashCode();
+        public Person() { }
+    }
+}
--- lesson 19/Program.cs
+++ lesson 19/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using lesson_19;
+
+Console.WriteLine("****Fun with System.Object*****\n");
+//эти обьекты идентичны
+Person p1 = new Person("Homer", "Simpson", 50, "111-11-1111");
+Person p2 = new Person("Homer", "Simpson", 50, "111-11-1111");
+
+//получить строковые версии обьектов
+Console.WriteLine("p1.ToString() = {0}", p1.ToString());
+Console.WriteLine("p2.ToString() = {0}", p2.ToString());
+
+//протестировать переопределенный метод Equals()
+Console.WriteLine("p1 = p2?: {0}", p1.Equals(p2));
+
+//протестировать хэщ коды
+//по прежнему используется хэш значение SSN
+Console.WriteLine("Same hash codes?: {0}", p1.GetHashCode() == p2.GetHashCode());
+
+Console.WriteLine();
+
+//изменить хначение Age обьекта p2 и протестировать снова 
+p2.Age = 45;
+Console.WriteLine("p1.ToString() = {0}", p1.ToString());
+Console.WriteLine("p2.ToString() = {0}", p2.ToString());
+Console.WriteLine("p1 = p2?: {0}", p1.Equals(p2));
+
+//по прежднему используется хэш значеение SSN
+Console.WriteLine("Same hash codes?: {0}", p1.GetHashCode() == p2.GetHashCode());
+Console.ReadLine();
+
+    
